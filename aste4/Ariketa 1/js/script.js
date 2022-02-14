a72d4b7d1d2a513b8777028d7ec18d54f84fa68a
JonAnderAsua <49275812+JonAnderAsua@users.noreply.github.com>
2022-02-14 14:21:28 +0100

    4. asteko ariketak

---
+++ aste4/Ariketa 1/js/script.js
@@ -0,0 +1,41 @@
+function inicializarGestores()
+{
+	var imagen = document.getElementById("imagen");
+	imagen.onclick = function()
+	{
+		alert("Has pulsado la imagen");
+	}
+
+	var usuario = document.getElementById("usuario");
+	usuario.value = 'tu@email';
+
+	usuario.onblur = function(){
+		if (usuario.value == ''){
+			usuario.value = "tu@email";
+		}
+	}
+
+	usuario.onfocus = function(){
+		if (usuario.value == 'tu@email'){
+			usuario.value = '';
+		}
+	}
+
+	var item = document.getElementById("combobox");
+	item.addEventListener("change",gestorCombo);
+
+	function gestorCombo(){
+		console.log(item.value);
+		console.log(item.options[item.selectedIndex].text);
+		console.log(item.selectedIndex);
+	} 
+
+	var formulario = document.getElementById('formulario');
+	formulario.onsubmit = function(){
+		console.log("click en submit");
+		return false;
+	}
+
+}
+
+window.onload = inicializarGestores;
---
+++ aste4/Ariketak 2 eta 3/oop/tienda.js
@@ -0,0 +1,12 @@
+import Producto from './productos.js';
+import Libro from './libros.js';
+import Disco from './discos.js'	;
+
+
+var libro1 = new Libro("Dublinés", "Alfonso Zapico", 18);
+var libro2 = new Libro("El arte de volar","Antonio Altarriba y Kim",20.90);
+var disco1 = new Disco("Próxima estación: Esperanza", "Manu Chao", 15, "CD");
+
+//var tienda = [];
+//tienda.push(libro1, libro2, disco1);
+export let tienda = [libro1,libro2,disco1];
